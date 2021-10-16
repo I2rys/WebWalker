fa3494cc4cf68a4f12137f6f7c06eff5b8d27520
Trac3D1y <i2rys@protonmail.com>
2021-10-16 16:52:55 -0700

    Updated! <3
    + Remove useless stuff
    + Added output
    + Fixed some typo

--- index.js
+++ index.js
@@ -6,50 +6,56 @@ const Fs = require("fs")
 const Self_Args = process.argv.slice(2)
 
 //Main
-if(Self_Args.length == 0){
-    console.log(`node index.js <url> <extensions>
-Example: node index.js https://google.com js,php,css`)
+if(!Self_Args.length){
+    console.log(`node index.js <output> <url> <extensions>
+Example: node index.js output_test.txt https://google.com js,php,css`)
     process.exit()
-}else{
-    if(Self_Args[0] == ""){
-        console.log("Invalid url.")
-        process.exit()
-    }
+}
 
-    if(Self_Args[0].indexOf("https") == -1){
-        console.log("Invalid url.")
-        process.exit()
-    }
+if(!Self_Args[0]){
+    console.log("Invalid output.")
+    process.exit()
+}
+
+if(!Self_Args[1]){
+    console.log("Invalid url.")
+    process.exit()
+}
+
+if(Self_Args[1].indexOf("https") == -1){
+    console.log("Invalid url.")
+    process.exit()
+}
 
     
-    if(Self_Args[1] == ""){
-        console.log("Invalid extensions.")
-        process.exit()
+if(!Self_Args[2]){
+    console.log("Invalid extensions.")
+    process.exit()
+}
+
+console.log("Scanning the URL for the extensions you specified. Please wait.")
+Main()
+async function Main(){
+    const config = {
+        host: Self_Args[1],
+        ext: Self_Args[2],
+        asyncRequests: true
     }
 
-    console.log("Scanning the URL for the extensions you specified. Please wait.")
-    Main()
-    async function Main(){
-        const config = {
-            host: Self_Args[0],
-            ext: Self_Args[1],
-            asyncRequests: true
-        }
-
-        const temp_results = await DirWalker.launch(config)
-        const temp_results2 = temp_results.founds.map((u) => (u.target))
-        var results = ""
+    const temp_results = await DirWalker.launch(config)
+    const temp_results2 = temp_results.founds.map((u) => (u.target))
+    var results = ""
         
-        for( i = 0; i <= temp_results2.length-1; i++ ){
-            if(results.length == 0){
-                results = temp_results2[i]
-            }else{
-                results += `\n${temp_results2[i]}`
-            }
+    for( i = 0; i <= temp_results2.length-1; i++ ){
+        if(!results.length){
+            results = temp_results2[i]
+        }else{
+            results += `\n${temp_results2[i]}`
         }
-
-        console.log("Successfully scanning please check output.txt")
-        Fs.writeFileSync("./output.txt", results, "utf8")
-        process.exit()
     }
+
+    console.log("Scanning is finished.")
+    Fs.writeFileSync(Self_Args[0], results, "utf8")
+    console.log(`The results has been saved to ${Self_Args[0]}`)
+    process.exit()
 }
